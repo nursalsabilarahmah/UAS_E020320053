680dcbd88f9d43ed0feaea33b60bb6e2d8706002
nursalsabilarahmah <cacabila582@gmail.com>
2023-01-15 00:27:10 +0700

    uas

---
+++ FDA_E020320053/app/src/main/java/id/ac/poliban/mi/vb/e020320053/fdae020320053/Activity/IntroActivity.java
@@ -0,0 +1,31 @@
+package id.ac.poliban.mi.vb.e020320053.fdae020320053.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import id.ac.poliban.mi.vb.e020320053.fdae020320053.R;
+import id.ac.poliban.mi.vb.e020320053.fdae020320053.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        setTitle("Salsabila - E020320053");
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
---
+++ FDA_E020320053/app/src/main/java/id/ac/poliban/mi/vb/e020320053/fdae020320053/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.e020320053.fdae020320053.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
